79e39fff84138b3c75d9ed88f7162fefe236bd99
ckouder <bingjiguo@icloud.com>
2024-11-11 09:09:56 -0600

    add query-input

--- src/app/components/query-input/query-input.component.ts
+++ src/app/components/query-input/query-input.component.ts
@@ -1,8 +1,21 @@
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenuModule } from 'primeng/menu';
+import { MarvinjsInputComponent } from '../marvinjs-input/marvinjs-input.component';
+
+interface SearchOption {
+  key: string;
+  label: string;
+  type: string;
+  formControl: FormControl<any | null>;
+  example: {
+    label: string;
+    value: string;
+  };
+}
 
 @Component({
   selector: 'app-query-input',
@@ -12,41 +25,89 @@ import { MenuModule } from 'primeng/menu';
     FormsModule,
     InputTextModule,
     MenuModule,
+    JsonPipe,
+    CommonModule,
+    MarvinjsInputComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './query-input.component.html',
   styleUrl: './query-input.component.scss'
 })
 export class QueryInputComponent {
-  searchConfigs = {
-    'compound': { 
+  searchConfigs: SearchOption[] = [
+    { 
+      key: 'compound',
       label: 'Compound', 
+      type: 'string',
       formControl: new FormControl<string | null>(''),
+      example: {
+        label: 'Ethanol (CCO)',
+        value: 'CCO'
+      },
     },
-    'organism': { 
+    { 
+      key: 'organism',
       label: 'Organism', 
+      type: 'string',
       formControl: new FormControl<string | null>(''),
+      example: {
+        label: 'Lentzea aerocolonigenes',
+        value: 'Lentzea aerocolonigenes'
+      }
     },
-    'uniprot_id': { 
+    { 
+      key: 'uniprot_id',
       label: 'Uniprot ID', 
+      type: 'string',
       formControl: new FormControl<string | null>(''),
+      example: {
+        label: 'A2BC19',
+        value: 'A2BC19'
+      }
     },
-    'ec_number': { 
+    { 
+      key: 'ec_number',
       label: 'EC Number', 
+      type: 'string',
       formControl: new FormControl<string | null>(''),
+      example: {
+        label: '2.7.10.1',
+        value: '2.7.10.1'
+      }
     },
-    'ph': { 
+    { 
+      key: 'ph',
       label: 'pH', 
+      type: 'range',
       formControl: new FormControl<[number, number] | null>(null),
+      example: {
+        label: '1-8',
+        value: '1-8'
+      }
     },
-    'temperature': { 
+    { 
+      key: 'temperature',
       label: 'Temperature', 
+      type: 'range',
       formControl: new FormControl<[number, number] | null>(null),
+      example: {
+        label: '37-39°C',
+        value: '37-39'
+      }
     },
-  };
-  searchOptions = Object.entries(this.searchConfigs).map(([key, config]) => ({
+  ];
+
+  readonly searchOptions = this.searchConfigs.map((config) => ({
     ...config,
     command: () => { this.selectedSearchOption = config }
   }));
-  selectedSearchOption: any = null;
+
+  readonly searchOptionRecords = this.searchConfigs.reduce((acc, config) => {
+    acc[config.key] = config;
+    return acc;
+  }, {} as Record<string, typeof this.searchConfigs[0]>);
+
+
+  selectedSearchOption: SearchOption | null = null;
 
 }
